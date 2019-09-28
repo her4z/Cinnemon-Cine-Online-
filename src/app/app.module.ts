a44bd0cfb32d29c78760befff4a211fa946423e8
Your Name <you@example.com>
2019-09-28 03:54:00 -0300

    cinnemon_alpha1.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { HeaderComponent } from './components/layout/header/header.component';
+import { IconComponent } from './components/buttons/icon/icon.component';
+import {MatButtonModule, MatDividerModule} from '@angular/material';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    IconComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatButtonModule,
+    MatDividerModule
   ],
   providers: [],
   bootstrap: [AppComponent]

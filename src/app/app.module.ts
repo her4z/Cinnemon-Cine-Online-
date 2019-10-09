c22060735da251256bc52ad6caa3db88e824374c
a <a>
2019-10-09 20:25:04 -0300

    routing en proceso - no ejecutable

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/layout/header/header.component';
 import { IconComponent } from './components/buttons/icon/icon.component';
 import {MatInputModule} from '@angular/material'
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {MatButtonModule} from '@angular/material';
+import { AppRoutingModule } from './app-routing.module';
+
+const appRoutes: Routes = [
+  { path: 'login', component: LogInComponent } ,
+  { path: 'search', component: SearchComponent}
+]
 @NgModule({
   declarations: [
     AppComponent,
@@ -18,7 +24,12 @@ import {MatButtonModule} from '@angular/material';
     MatInputModule,
     FormsModule,
     ReactiveFormsModule,
-    MatButtonModule
+    MatButtonModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true }
+    ),
+    AppRoutingModule
     
   ],
   providers: [],
